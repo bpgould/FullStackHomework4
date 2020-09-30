619dead4cb389f21151a7287d86a03c5f839c09f
Bennett Gould <bgould@protonmail.com>
2020-09-30 01:14:11 -0400

    added Question Object and toString

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 $(document).ready(function(){
     var questions = [];
+    var qAndA = [
+        "question  1",
+        "answer 1",
+        "answer 2",
+        "answer 3",
+        "answer 4",
+        "question 2",
+        "answer 11",
+        "answer 22",
+        "answer 33",
+        "answer 44",
+    ];
 
-    class Question{
-        constructor(q, first, second, third, fourth){
-            this.question = q;
-            this.firstQuestion = first;
-            this.secondQuestion = second;
-            this.thirdQuestion = third;
-            this.fourthQuestion = fourth;
+    
+        function Question(question, firstQuestion, secondQuestion, thirdQuestion, fourthQuestion){
+            this.question = question;
+            this.firstQuestion = firstQuestion;
+            this.secondQuestion = secondQuestion;
+            this.thirdQuestion = thirdQuestion;
+            this.fourthQuestion = fourthQuestion;
         }
+        // added toString for easier debugging
+        Question.prototype.toString = function questionToString(){
+            return this.question;
+        }
+    
+    var counter = 0;
+    for(i=0;i<qAndA.length-4;i+=5){
+        let x = new Question(qAndA[i],qAndA[i+1],qAndA[i+2],qAndA[i+3],qAndA[i+4]);
+        questions[counter]=x;
+        counter++;
     }
+
     
     $("#startQuizButton").click(function(){
         $("#hiddenOncePlayed").css('display','none')
@@ -38,12 +61,12 @@ $(document).ready(function(){
             '<li id="fourthElement"></li>'
         ).addClass('list-group-item');
 
-        startQuiz();
+        populateQuiz();
     });
 
-    function startQuiz(){
+    function populateQuiz(){
         
-
+        // get question and randomize order that answers are displayed
        
     }
 
